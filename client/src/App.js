a4fc63163ec9f2d01a4d5869cf677d0302969fab
John Matyas <22-7517-1@plus4u.net>
2023-06-08 00:36:27 +0200

    Skeleton is Functional, Error at API level
    The server part will most probably be needing some updates, skeleton is up to date.

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import ReceptInfo from './bricks/ReceptInfo';
+import ReceptList from "./bricks/ReceptList";
+import Icon from "@mdi/react";
+import { mdiLoading } from "@mdi/js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./css/cookbooke.module.css";
+
+function App() {
+  const [cookbookLoadCall, setCookbookLoadCall] = useState({
+    state: "pending",
+  });
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/cookbook/load?id=${"f780b198cf290778"}`, {
+      method: "GET",
+    }).then(async (response) => {
+      const responseJson = await response.json();
+      if (response.status >= 400) {
+        setCookbookLoadCall({ state: "error", error: responseJson });
+      } else {
+        setCookbookLoadCall({ state: "success", data: responseJson });
+      }
+    });
+  }, []);
+
+  function getIngredients() {
+    switch (cookbookLoadCall.state) {
+      case "pending":
+        return (
+          <div className={styles.loading}>
+            <Icon size={2} path={mdiLoading} spin={true} />
+          </div>
+        );
+      case "success":
+        return (
+          <>
+            <ReceptInfo cookbook={cookbookLoadCall.data} />
+            <ReceptList receptList={cookbookLoadCall.data} />
+          </>
+        );
+      case "error":
+        return (
+        <div className={styles.error}>
+          <div>Nepodařilo se načíst data o třídě.</div>
+          <br />
+          <pre>{JSON.stringify(cookbookLoadCall.error, null, 2)}</pre>
+        </div>
+      );
+      default:
+        return null;
+    }
+  }
+
+  return <div className="App">{getIngredients()}</div>;
+}
+
+export default App;
